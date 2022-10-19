bc3ca4edb58f0d0e491c932d174c0c8ffa13a4e9
Joao_Guima <jotam24@hotmail.com>
2022-10-19 21:24:00 +0100

    Reading the tours from json file . Showing them to the GET (Routing: /api/v1/tours)

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -1,11 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  //send message
-  //res.status(200).send('Hello World!');
-  res.status(200).json({ message: 'Helllo World!' });
+//we are getting the tours from the json file with (fs)
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    //we should pass the status and the number of results in the json data
+    status: 'success',
+    results: tours.length,
+    //then we give the data
+    data: {
+      //because the key and the value has the same name, we can just give the value
+      tours,
+    },
+  });
 });
 
 app.post('/', (req, res) => {
