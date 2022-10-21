09879aa54d5e2c505d255aa3eea775de2ad83bad
Joao_Guima <jotam24@hotmail.com>
2022-10-21 16:20:46 +0100

    implementing the mongoDB connection: Mongoose Framework

--- NATOURS/server.js
+++ NATOURS/server.js
@@ -1,7 +1,25 @@
 const dotenv = require('dotenv');
 
+//Importing mongoose framework
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+//Creating the string to connect to DB (replacing the password)
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//Connecting to the db
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log('MongoDB connected !'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
