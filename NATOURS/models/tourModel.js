7dfe43ff5ace72788d8da512a15312140e1824d4
Joao_Guima <jotam24@hotmail.com>
2022-10-22 14:56:43 +0100

    refactor the tour model

--- NATOURS/models/tourModel.js
+++ NATOURS/models/tourModel.js
@@ -8,12 +8,56 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'The name of the tour is mandatory.'],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    requires: [true, 'Duration is mandatory'],
+  },
+  difficulty: {
+    type: String,
+    requires: [true, 'Difficulty is mandatory'],
+  },
+  maxGroupSize: {
+    type: Number,
+    requires: [true, 'GroupSize is mandatory'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   rating: Number,
   price: {
     type: Number,
     required: [true, 'The price of the tour is mandatory'],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    required: [true, 'The summary of the tour is mandatory'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    requires: [true, 'Description is mandatory'],
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'The image of the tour is mandatory'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 //Creating our model
