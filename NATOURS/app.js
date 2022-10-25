2a949f5f7da3109aea6987e78e0e1ea6107d7440
Joao_Guima <jotam24@hotmail.com>
2022-10-25 21:52:13 +0100

    Implementing the rate limit: protecting our api to a massive request

--- NATOURS/app.js
+++ NATOURS/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const errorController = require('./controllers/errorController');
 
@@ -9,11 +10,25 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//middleware
+//Global middleware
 if (process.env.NODE_ENV === 'development') {
   //using the logger but only in the development env.
   app.use(morgan('dev'));
 }
+
+//Implementing the limiter global middleware, so you can only do X requests per hour
+
+const limiter = rateLimit({
+  //max requests per ip
+  max: 100,
+  //in time (ms)
+  windowsMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later',
+});
+
+//passing the middleware to every request to our routes starting in '/api'
+app.use('/api', limiter);
+
 app.use(express.json());
 
 //serving static files (public folder open to a static endpoint)
